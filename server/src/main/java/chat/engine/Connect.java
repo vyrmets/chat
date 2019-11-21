c6d03e2d1e9463d28ae910e81e15eb76ce7655e9
Serhii Onyshchenko <serhii.onyshchenko@thomsonreuters.com>
2019-11-22 01:19:04 +0200

    Refactoring, add runnable connection for multiple clients

---
+++ server/src/main/java/chat/engine/Connect.java
@@ -0,0 +1,60 @@
+package chat.engine;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Connect implements Runnable {
+
+    private final static Logger LOGGER = Logger.getLogger(Server.class);
+
+    private Socket socket;
+
+    private BufferedWriter writer;
+
+    private BufferedReader reader;
+
+    public Connect(Socket socket) throws IOException {
+        this.socket = socket;
+        writer = getWriter(socket);
+        reader = getReader(socket);
+    }
+
+    @Override
+    public void run() {
+        LOGGER.info("Client connected");
+
+        while (true) {
+            try {
+                String message = reader.readLine();
+                LOGGER.info(message);
+
+                writer.write(message + "\r\n");
+                writer.flush();
+            } catch (IOException e) {
+                LOGGER.error("Failed to read message");
+                try {
+                    socket.close();
+                    reader.close();
+                    writer.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private BufferedReader getReader(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        return new BufferedReader(inputStreamReader);
+    }
+
+    private BufferedWriter getWriter(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        return new BufferedWriter(outputStreamWriter);
+    }
+}
--- server/src/main/java/chat/engine/Server.java
+++ server/src/main/java/chat/engine/Server.java
@@ -2,57 +2,25 @@ package chat.engine;
 
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
 
 public class Server {
 
-    private final Logger logger = Logger.getLogger(Server.class);
+    private final static Logger LOGGER = Logger.getLogger(Server.class);
+
+    private final static int PORT = 8080;
 
     public void run() throws IOException {
-        logger.info("Server starting...");
+        LOGGER.info("Server started at port: " + PORT);
 
-        try (ServerSocket serverSocket = new ServerSocket(8000)) {
-
-            while (true) {
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
                 Socket socket = serverSocket.accept();
-                OutputStream outputStream = socket.getOutputStream();
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, Charset.forName("UTF-8"));
-                BufferedWriter writer = new BufferedWriter(outputStreamWriter);
-
-                InputStream inputStream = socket.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        logger.info("Client connected");
-                        String a = null;
-                        try {
-                            while (true) {
-                                a = reader.readLine();
-                                logger.info(a);
-
-                                writer.write(a + "\r\n");
-
-                                writer.flush();
-                            }
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                socket.close();
-                                serverSocket.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                });
+                new Thread(new Connect(socket)).start();
+            } catch (IOException e) {
+                LOGGER.error("Failed to start server: ", e);
             }
         }
     }
